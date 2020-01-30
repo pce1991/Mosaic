5fdb88ee4539a5db2f54ea424cdef414f9550c5c
unknown <patrick.collin.eye@gmail.com>
2020-01-30 13:19:26 -0600

    lin

--- compile_linux.bash
+++ compile_linux.bash
@@ -1,4 +1,4 @@
 
 rm -rf build/
 mkdir build/
-gcc -O0 -Isrc -Ilib src/linux.cpp -o game -lX11 -lGL -lGLU -lGLEW -lm -std=c++11 -w
+gcc -O0 -Isrc -Ilib src/linux.cpp -o game -lX11 -lGL -lGLU -lGLEW -lm -lpthread -std=c++11 -w
