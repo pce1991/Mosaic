56a28fc6c2c2bc194a41ad5e79fe8e8ee936817e
unknown <patrick.collin.eye@gmail.com>
2020-02-01 20:01:13 -0600

    started linux shaders for pi

--- src/linux.cpp
+++ src/linux.cpp
@@ -308,7 +308,7 @@ void InitOpenGL(Display *display, Window *window, GLXContext *context) {
   
     int contextAttributes[] =
         {
-            GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
+            GLX_CONTEXT_MAJOR_VERSION_ARB, 2,
             GLX_CONTEXT_MINOR_VERSION_ARB, 1,
             //GLX_CONTEXT_FLAGS_ARB        , GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
             None
