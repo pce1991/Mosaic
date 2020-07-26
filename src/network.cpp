b603c558cb7786d67aa18d3744cd9fdb8d1a2a7d
pce <patrick.collin.eye@gmail.com>
2020-07-26 00:16:26 -0500

    nevermind

--- src/network.cpp
+++ src/network.cpp
@@ -36,8 +36,7 @@ uint32 InitSocket(Socket *socketPtr, uint32 address, int16 port) {
     socketPtr->address = address;
 
     addr->sin_family = AF_INET;
-    //addr->sin_addr.s_addr = htonl(address);
-    addr->sin_addr.s_addr = address;
+    addr->sin_addr.s_addr = htonl(address);
     addr->sin_port = htons(port);
 
     int32 bindSuccess = bind(socketPtr->handle, (const sockaddr*)addr, sizeof(sockaddr_in));
