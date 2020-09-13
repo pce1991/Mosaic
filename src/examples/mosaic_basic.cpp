a72b2e5bbcd77d7db3fce4fc45a18cca40a0b274
pce <patrick.collin.eye@gmail.com>
2020-09-12 22:55:15 -0500

    fixed mosaic_basic

--- src/examples/mosaic_basic.cpp
+++ src/examples/mosaic_basic.cpp
@@ -32,72 +32,20 @@ void MyMosaicInit() {
     Log("color r %f g %f b %f", Mosaic->screenColor.r, Mosaic->screenColor.g, Mosaic->screenColor.b);
 }
 
-
-real32 timeSpacePressed = 0.0f;
-
-int32 keyframeIndex = 0;
-
-real32 red = 0.0f;
-
 // This is where you put the code you want to run every update.
 // This function is called every frame, and its what tells us what colors to draw
 // all the tiles at, along with all the other state changes in our game.
 void MyMosaicUpdate() {
     Tile *tiles = Mosaic->tiles;
 
-    // We want the keyframe index to change every n seconds
-    // Which means we need a way to record how long its been since the last keyframe
-    // (which means we need to know the time we started the last keyframe)
-    // Then if its been long enough we goto the next keyframe.
-    // Look at the example of the spacebar, instead of based on when the spacebar
-    // see if you can find a way to base it on how many seconds since the keyframeIndex
-    // has changed.
-
-    if (InputPressed(Input, Input_Space)) {
-        keyframeIndex++;
-    }
-
-    if (InputPressed(Input, Input_RightArrow)) {
-        red = red + 0.1;
-    }
-    if (InputPressed(Input, Input_LeftArrow)) {
-        red = red - 0.1;
-    }
-
-    // We have two things going on: we have a animation that changes based on time
-    // and then we augment that animation based on our input.
-
     for (int y = 0; y < Mosaic->gridHeight; y++) {
         for (int x = 0; x < Mosaic->gridWidth; x++) {
             Tile *tile = GetTile(x, y);
             tile->color = RGB(0.0f, 0.0f, 0.0f);
-
-            if (keyframeIndex == 0) {
-                tile->color = RGB(1.0f + red, 0.0f, 0.0f);
-            }
-            if (keyframeIndex == 1) {
-                tile->color = RGB(0.0f + red, 1.0f, 0.0f);
-            }
         }
     }
-
-    // To make a variable we give it the type
-    // This tells us how big the data and how to interpret it.
-    real32 t = 1.0f;
-
-    if (InputPressed(Input, Input_Space)) {
-        timeSpacePressed = Game->time;
-    }
-    
-
-    // The "arrow" operator -> is a way to access members of a pointer
-    real32 timeSinceKeyframe = Game->time - timeKeyframeChanged;
 
     if (Mosaic->hoveredTile) {
         Mosaic->hoveredTile->color = V4(1, 1, 1, 1);
-
-        if (timeSincePressedSpace >= 5) {
-            Mosaic->hoveredTile->color = V4(1, 0, 0, 1);
-        }
     }
 }
