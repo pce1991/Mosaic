ba2395c35da4c328a80fdaf9804f820518b0cfbd
pce <patrick.collin.eye@gmail.com>
2021-04-01 21:43:28 -0500

    guy

--- src/examples/mosaic_tile_renderer.cpp
+++ src/examples/mosaic_tile_renderer.cpp
@@ -1,5 +1,8 @@
 
-const int32 tileWidth = 8;
+// @TODO: start with a much simpler hardcoded example to illustrate the difficulties
+// we'd have if we just didnt have these abstractions
+
+const int32 tileWidth = 5;
 const int32 tileSize = tileWidth * tileWidth;
 
 // @TODO: call this a sprite instead. 
@@ -12,12 +15,43 @@ int32 height = 8;
 
 vec2 pos = V2(0, 0);
 
+Tile playerTile = {};
+
 Tile grassTile = {};
 Tile grassTile2 = {};
 
 void MyMosaicInit() {
     SetMosaicGridSize(width * tileWidth, height * tileWidth);
     HideGrid();
+
+    {
+        for (int i = 0; i < tileSize; i++) {
+            playerTile.colors[i] = V4(0);
+        }
+        
+        vec4 color = RGB(0.7f, 0.0f, 0.2f);
+        color.a = 1;
+        
+        playerTile.colors[1 + (4 * tileWidth)] = color;
+        playerTile.colors[3 + (4 * tileWidth)] = color;
+
+        playerTile.colors[1 + (3 * tileWidth)] = color;
+        playerTile.colors[3 + (3 * tileWidth)] = color;
+
+        playerTile.colors[1 + (2 * tileWidth)] = color;
+        playerTile.colors[2 + (2 * tileWidth)] = color;
+        playerTile.colors[3 + (2 * tileWidth)] = color;
+
+        playerTile.colors[0 + (1 * tileWidth)] = color;
+        playerTile.colors[1 + (1 * tileWidth)] = color;
+        playerTile.colors[2 + (1 * tileWidth)] = color;
+        playerTile.colors[3 + (1 * tileWidth)] = color;
+        playerTile.colors[4 + (1 * tileWidth)] = color;
+
+        playerTile.colors[1 + (0 * tileWidth)] = color;
+        playerTile.colors[2 + (0 * tileWidth)] = color;
+        playerTile.colors[3 + (0 * tileWidth)] = color;
+    }
 
     for (int i = 0; i < tileSize; i++) {
         grassTile.colors[i] = RGB(0.1f, RandfRange(0.6f, 1.0f), RandfRange(0.2f, 0.4f));
@@ -40,7 +74,11 @@ void DrawTile(int32 posX, int32 posY, Tile *tile) {
     
     for (int y = posY; y < posY + tileWidth; y++) {
         for (int x = posX; x < posX + tileWidth; x++) {
-            SetTileColor(x, y, tile->colors[index]);
+            // @NOTE: doing this because we're overwriting whatever tile is there which
+            // means we wouldnt wind up blending between that tile and nothing.
+            if (tile->colors[index].a > 0) {
+                SetTileColor(x, y, tile->colors[index]);
+            }
             index++;
         }
     }
@@ -49,6 +87,7 @@ void DrawTile(int32 posX, int32 posY, Tile *tile) {
 void DrawTile(int32 posX, int32 posY, vec4 color) {
     for (int y = posY; y < posY + tileWidth; y++) {
         for (int x = posX; x < posX + tileWidth; x++) {
+            color.a = 0;
             SetTileColor(x, y, color);
         }
     }
@@ -57,7 +96,7 @@ void DrawTile(int32 posX, int32 posY, vec4 color) {
 void MyMosaicUpdate() {
     ClearTiles(0, 0, 0);
 
-    real32 speed = 50;
+    real32 speed = 40;
     if (InputHeld(Input, Input_Left)) {
         pos.x += -speed * DeltaTime;
     }
@@ -86,5 +125,6 @@ void MyMosaicUpdate() {
         }
     }
     
-    DrawTile(pos.x, pos.y, RGB(0.8f, 0.0f, 0.2f));
+    //DrawTile(pos.x, pos.y, RGB(0.8f, 0.0f, 0.2f));
+    DrawTile(pos.x, pos.y, &playerTile);
 }
