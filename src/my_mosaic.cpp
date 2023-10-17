2014ea94929e038fb00a54a2258527e9e6d167bf
unknown <patrick.collin.eye@gmail.com>
2023-10-17 15:37:55 -0500

    foose ball update

--- src/my_mosaic.cpp
+++ src/my_mosaic.cpp
@@ -9,15 +9,23 @@
 //@TODO: definite piece position as offset on axel so that they're always consistent and we don't
 // get the jitter on some pieces
 
-const int32 PieceSize = 5;
+// @TODO: what if game was about keeping the ball from reaching either side rather than scoring a goal? 
+
+const int32 InputLength = 8;
 
 struct Player {
     int32 inputIndex;
-    char input[8];
+    char input[InputLength];
 };
+
+const int32 PieceSize = 5;
+real32 CollisionDuration = 0.5f;
 
 struct Piece {
     vec2 position;
+
+    bool collisionActive;
+    real32 timeCollisionActivated;
 };
 
 // @TOOD: get the pieces
@@ -37,7 +45,7 @@ struct Ball {
 };
 
 struct Axel {
-    char string[8];
+    char string[InputLength];
 
     int32 x;
     vec2 velocity;
@@ -68,6 +76,13 @@ const int32 ArenaHeight = 80;
 struct GameMem Foose = {};
 
  
+void GenerateAxelString(char *string) {
+    string[0] = 'a';
+    string[1] = 'b';
+    string[2] = 'c';
+}
+
+
 void StartRound() {
     Ball *ball = &Foose.ball;
 
@@ -75,6 +90,17 @@ void StartRound() {
     
     real32 speed = 20;
     ball->velocity = Rotate(DegToRad(RandfRange(-30, 30)), V2(-1, 0)) * speed;
+
+    for (int t = 0; t < 2; t++) {
+        Team *team = &Foose.teams[t];
+
+        // find which direction we should move to have one of our pieces intercept the ball
+        for (int a = 0; a < AxelCount; a++) {
+            Axel *axel = &team->axels[a];
+
+            GenerateAxelString(axel->string);
+        }
+    }
 }
 
 
@@ -98,6 +124,8 @@ void MyMosaicInit() {
     
     {
         Team *team = &Foose.teams[0];
+        *team = {};
+        
         team->color = V4(0.7f, 0.2f, 0.0f, 1.0f);
 
         {
@@ -183,10 +211,17 @@ void RenderTeam(Team *team) {
         Axel *axel = &team->axels[a];
 
         vec2 textPos = V2(axel->x, 0);
-        DrawTextTile(textPos, 2.0, V4(1), "abc");
+        DrawTextTile(textPos, 2.0f, V4(1), axel->string);
 
         for (int j = 0; j < axel->pieceCount; j++) {
-            DrawPiece(&axel->pieces[j], team->color);
+            Piece *piece = &axel->pieces[j];
+            vec4 color = team->color;
+
+            if (piece->collisionActive) {
+                color = V4(1, 0, 0, 1);
+            }
+            
+            DrawPiece(&axel->pieces[j], color);
         }
     }
 }
@@ -194,45 +229,80 @@ void RenderTeam(Team *team) {
 void MyMosaicUpdate() {
     ClearTiles(0.1f, 0.3f, 0.15f);
 
-    if (Input->charCount > 0) {
-        char input = Input->inputChars[0];
-
+    {
         Player *player = &Foose.player;
-        player->input[player->inputIndex] = input;
-        player->inputIndex++;
-    }
 
-    {
-        Ball *ball = &Foose.ball;
-
-        ball->position = ball->position + ball->velocity * DeltaTime;
-
-        // check for collisions
-        vec2 min = ball->position;
-        vec2 max = ball->position + V2(BallSize);
-
-        if (min.y < 0) {
-            ball->velocity.y *= -1;
+        // @TODO: if you get a match we should also clear it
+        
+        if (InputPressed(Keyboard, Input_Return)) {
+            for (int i = 0; i < InputLength; i++) {
+                player->input[i] = 0;
+            }
+            
+            player->inputIndex = 0;
         }
-        else if (max.y >= ArenaHeight) {
-            ball->velocity.y *= -1;
-        }
-        else if (min.x < 0) {
-            ball->velocity.x *= -1;
-        }
-        else if (max.x >= ArenaWidth) {
-            ball->velocity.x *= -1;
+        else {
+            if (Input->charCount > 0) {
+                char input = Input->inputChars[0];
+
+                if (player->inputIndex < InputLength) {
+                    player->input[player->inputIndex] = input;
+                    player->inputIndex++;
+                }
+            }
         }
     }
 
+
     {
+        Player *player = &Foose.player;
         Ball *ball = &Foose.ball;
         for (int t = 0; t < 2; t++) {
             Team *team = &Foose.teams[t];
 
+            bool foundMatch = false;
+            
             // find which direction we should move to have one of our pieces intercept the ball
             for (int a = 0; a < AxelCount; a++) {
                 Axel *axel = &team->axels[a];
+
+                // activate collision
+                {
+                    int32 axelStringLength = 0;
+
+                    for (int32 c = 0; c < InputLength; c++) {
+                        if (axel->string[c] != 0) {
+                            axelStringLength++;
+                        }
+                    }
+
+                    bool matches = axelStringLength == player->inputIndex;
+                                        
+                    for (int32 c = 0; c < player->inputIndex; c++) {
+                        if (axel->string[c] != player->input[c]) {
+                            matches = false;
+                        }
+                    }
+
+                    if (matches) {
+                        for (int32 i = 0; i < axel->pieceCount; i++) {
+                            Piece *piece = &axel->pieces[i];
+                            piece->collisionActive = true;
+                            piece->timeCollisionActivated = Time;
+                        }
+
+                        foundMatch = true;
+                    }
+                }
+
+                for (int32 i = 0; i < axel->pieceCount; i++) {
+                    Piece *piece = &axel->pieces[i];
+                    real32 elapsed = Time - piece->timeCollisionActivated;
+
+                    if (elapsed >= CollisionDuration) {
+                        piece->collisionActive = false;
+                    }
+                }
 
                 real32 closestY = 999999;
                 real32 dirY = 0;
@@ -285,12 +355,88 @@ void MyMosaicUpdate() {
                     piece->position.y -= borderOverlap;
                 }
             }
+
+            if (foundMatch) {
+                // reset input for everything
+                for (int i = 0; i < InputLength; i++) {
+                    player->input[i] = 0;
+                }
+            
+                player->inputIndex = 0;
+
+                // @TODO: regenerate the strings for axels
+            }
+        }
+    }
+
+    {
+        Ball *ball = &Foose.ball;
+
+        ball->position = ball->position + ball->velocity * DeltaTime;
+
+        // check for collisions
+        vec2 min = ball->position;
+        vec2 max = ball->position + V2(BallSize);
+
+        if (min.y < 0) {
+            ball->velocity.y *= -1;
+        }
+        else if (max.y >= ArenaHeight) {
+            ball->velocity.y *= -1;
+        }
+        else if (min.x < 0) {
+            ball->velocity.x *= -1;
+        }
+        else if (max.x >= ArenaWidth) {
+            ball->velocity.x *= -1;
+        }
+
+        // check collision against player
+        for (int t = 0; t < 2; t++) {
+            Team *team = &Foose.teams[t];
+
+            // find which direction we should move to have one of our pieces intercept the ball
+            for (int a = 0; a < AxelCount; a++) {
+                Axel *axel = &team->axels[a];
+
+                for (int32 i = 0; i < axel->pieceCount; i++) {
+                    Piece *piece = &axel->pieces[i];
+
+                    vec2 pieceMin = piece->position;
+                    vec2 pieceMax = piece->position + V2(PieceSize);
+
+                    if (!piece->collisionActive) { continue; }
+
+                    vec2 dir;
+                    if (TestAABBAABB(min, max, pieceMin, pieceMax, &dir)) {
+                        ball->position = ball->position + dir;
+
+                        real32 force = 1.2f;
+                        if (dir.x < 0) {
+                            ball->velocity.x *= -1;
+                            //ball->velocity.x *= force;
+                        }
+                        else if (dir.x > 0) {
+                            ball->velocity.x *= -1;
+                            //ball->velocity.x *= force;
+                        }
+                        else if (dir.y < 0) {
+                            ball->velocity.y *= -1;
+                            //ball->velocity.y *= force;
+                        }
+                        else if (dir.x > 0) {
+                            ball->velocity.y *= -1;
+                            //ball->velocity.y *= force;
+                        }
+                    }
+                }
+            }
         }
     }
 
     {
         Player *player = &Foose.player;
-        DrawTextTop(V4(1), "%.*s", player->inputIndex, player->input);
+        DrawTextTile(V2(ArenaWidth / 2, ArenaHeight + 1), 5.0f, V4(1), false, "%.*s", player->inputIndex, player->input);
     }
 
     RenderTeam(&Foose.teams[0]);
