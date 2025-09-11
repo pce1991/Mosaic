a8d76bb0f39e3df2aade798005440f64233e7f2e
pce1991 <patrick.collin.eye@gmail.com>
2025-09-10 20:25:26 -0500

    PrintNoLine

--- src/windows_api.cpp
+++ src/windows_api.cpp
@@ -19,6 +19,22 @@ void Print(const char *fmt, ...) {
 }
 
 
+void PrintNoLine(const char *fmt, ...) {
+    char buffer[PRINT_MAX_BUFFER_LEN];
+
+    va_list args;
+    va_start (args, fmt);
+
+
+    vsnprintf(buffer, PRINT_MAX_BUFFER_LEN, fmt, args);
+
+    OutputDebugString(buffer);
+
+    va_end(args);
+}
+
+
+
 void WinMoveMouse(HWND window, int32 screenX, int32 screenY, int32 height) {
     POINT screenPoint;
     screenPoint.x = screenX;
