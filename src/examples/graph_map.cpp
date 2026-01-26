e1dab548c12eca448a48cbb0f4d9920656d8e36b
pce1991 <patrick.collin.eye@gmail.com>
2026-01-25 19:16:12 -0600

    added 2026 VS path

---
+++ src/examples/graph_map.cpp
@@ -0,0 +1,20 @@
+// Manual can be found here:
+// https://github.com/pce1991/MosaicBook#readme
+
+struct MapNode {
+
+};
+
+struct Map {
+  DynamicArray<MapNode> nodes;
+  
+};
+
+
+void MyGameInit() {
+
+}
+
+void MyGameUpdate() {
+  
+}
