24f9e76ecb01738cb583535d5afaf9d2f3c9e2d2
pce1991 <patrick.collin.eye@gmail.com>
2026-08-21 16:54:50 -0500

    my_mosaic.cpp empty now

--- src/game/my_mosaic.cpp
+++ src/game/my_mosaic.cpp
@@ -1,43 +1,8 @@
 
-
-// Pick which example runs by uncommenting the include you want.
-//#include "../examples/synth_test.cpp"
-#include "../examples/mosaic_pretty_tiles.cpp"
-//#include "examples/resolution.cpp"
-
-// The original UI demo code that used to live here, kept around in case you
-// want to switch back:
-#if 0
 void MyMosaicInit() {
-  Mosaic->drawBorder = true;
-}
 
-char nameInput[32] = "edit me";
+}
 
 void MyMosaicUpdate() {
-  SetTileColor(0, 0, 0.45f, 0.3f, 0.3f);
-  SetTileColor(1, 0, 0.2f, 0.6f, 0.4f);
 
-  UIBegin();
-
-  UIPushWindow(V2(140, 100), V2(700, 400), V4(0.15f, 0.15f, 0.18f, 0.95f), NULL);
-
-  UILabel("Hello");
-
-  if (UIButton("Hello")) {
-  }
-
-  UIStyle fieldStyle = UICopyStyle();
-  fieldStyle.buttonColor       = V4(0.9f, 0.9f, 0.92f, 1.0f);
-  fieldStyle.buttonHoverColor  = V4(0.94f, 0.94f, 0.97f, 1.0f);
-  fieldStyle.buttonActiveColor = V4(0.75f, 0.88f, 1.0f, 1.0f);
-  fieldStyle.textColor         = V4(0.1f, 0.12f, 0.16f, 1.0f);
-  UIPushStyle(fieldStyle);
-  if (UIStringField("name_input", nameInput, sizeof(nameInput))) {
-      // Enter committed the edit
-  }
-  UIPopStyle();
-
-  UIPopWindow();
 }
-#endif
